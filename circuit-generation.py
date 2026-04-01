dea259b22b8e18bb034ca2ec15446221f7d9bd24
hazel <ctorek@clemson.edu>
2026-04-01 13:55:14 -0400

    add the other decision variables

--- circuit-generation.py
+++ circuit-generation.py
@@ -1,43 +1,54 @@
 from qiskit import QuantumCircuit
 from qiskit.circuit.library import quantum_volume
 
+from docplex.mp.model import Model
+from functools import reduce
+import itertools as it
+
 # create hardware topology just as a set of edges
-top_L6 = {(1, 2), (2, 3), (3, 4), (4, 5), (5, 6)}
-top_Y6 = {(1, 2), (2, 3), (3, 6), (3, 4), (4, 5)}
-top_G6 = {(1, 2), (2, 3), (4, 5), (5, 6), (1, 4), (2, 5), (3, 6)}
-top_L8 = {(1, 2), (2, 3), (3, 4), (4, 5), (5, 6), (6, 7), (7, 8)}
-top_Y8 = {(1, 2), (2, 3), (3, 4), (4, 5), (5, 6), (4, 7), (7, 8)}
-top_G8 = {(1, 2), (2, 3), (3, 4), (5, 6), (6, 7), (7, 8), (1, 5), (2, 6), (3, 7), (4, 8)}
+topologies = {
+    "L6": {(1, 2), (2, 3), (3, 4), (4, 5), (5, 6)},
+    "Y6": {(1, 2), (2, 3), (3, 6), (3, 4), (4, 5)},
+    "G6": {(1, 2), (2, 3), (4, 5), (5, 6), (1, 4), (2, 5), (3, 6)},
+    "L8": {(1, 2), (2, 3), (3, 4), (4, 5), (5, 6), (6, 7), (7, 8)},
+    "Y8": {(1, 2), (2, 3), (3, 4), (4, 5), (5, 6), (4, 7), (7, 8)},
+    "G8": {(1, 2), (2, 3), (3, 4), (5, 6), (6, 7), (7, 8), (1, 5), (2, 6), (3, 7), (4, 8)}
+}
+
+# make sure edges are oriented bidirectionally 
+for k in topologies.keys():
+    # add the reverse orientation of each edge to the set as well
+    topologies[k] = topologies[k].union(set((j, i) for (i, j) in list(topologies[k])))
 
 # change for different runs
-topology = top_L6
+topology = topologies["L6"]
 num_qubits = 6
 
 # generate a 6-qubit example circuit
 circuit = quantum_volume(num_qubits = num_qubits, depth = num_qubits)
 
 # https://github.com/IBMDecisionOptimization/docplex-examples/blob/master/examples/mp/jupyter/tutorials/Linear_Programming.ipynb
-from docplex.mp.model import Model
-from functools import reduce
-import itertools as it
-
 m = Model(name = "circuit-optimizer")
 
 # setting up decision variables described in 3.1 > Location Variables and Constraints
-Q = range(num_qubits + 1)      # 6 qubits in the example circuit
-V = range(num_qubits + 1)      # 6 qubits in the hardware topology
-T = range(num_qubits * 5 + 1)  # 4 dummy and 1 real timestep for each depth, plus one for exclusive index
+Q = range(num_qubits)      # 6 qubits in the example circuit
+V = range(num_qubits)      # 6 qubits in the hardware topology
+T = range(num_qubits * 5)  # 4 dummy and 1 real timestep for each depth
 
 # https://ibmdecisionoptimization.github.io/docplex-doc/mp/docplex.mp.model.html#docplex.mp.model.Model.binary_var
+# w_(q, i, t) = 1 iff qubit q resides at node i (in hardware) at time t, described under 3.1 > Location
 w = m.binary_var_cube(Q, V, T, name = "w")
+m.add_constraints(m.sum(w[q, v, t] for v in V) == 1 for (q, t) in it.product(Q, T)) # each qubit is at exactly one node
+m.add_constraints(m.sum(w[q, v, t] for q in Q) == 1 for (v, t) in it.product(V, T)) # each node hosts exactly one qubit
 
-# setting up decision variables described in 3.1 > Gate Variables and Constraints
-# need to define this one separately for each timestep t since the set of gates is indexed by t
-G = list(it.chain(*(list(it.repeat((), 4)) + [g] for g in [tuple(circuit.find_bit(q).index for q in circuit.data[t].qubits) for t in range(num_qubits + 1)])))
-H = list(it.product(range(num_qubits + 1), range(num_qubits + 1)))
-y = m.binary_var_cube(G, H, T, name = "y")
+# y_((p,q), (i, j), t) = 1 iff gate (p, q) is impl on hardware arc (i, j) at time t
+G = [tuple(circuit.find_bit(q).index for q in circuit.data[t].qubits) for t in V] # logical gates at timestep t
+y = [m.binary_var_matrix(G[t], topology, name = f"y{t}") for t in V]
 
-# constraint: each gate is impl. once
-for (t, g) in it.product(T, G):
-    if g is (): continue
-    m.add_constraint(reduce(lambda x, y: x + y, [y[g][h][t] for h in topology]))
+# TODO: still need to add constraints on y, this is the bit with mccormick constraints
+
+# x_(q, i, j, t) = 1 iff qubit q is located at node i at time t and located at node j at time t+1
+N = [{(i, j) for (i, j) in topology if i == n} for n in V]
+x = m.binary_var_dict((q, i, j, t) for t in T[:-1] for i in V for j in N[i] for q in Q)
+
+# TODO: still need to add constraints on x, see page 7  
